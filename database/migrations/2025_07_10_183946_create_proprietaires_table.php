3b7ab46e72c6f4ae3e4779821fcf43d80f237da1
AIT-MOHAMED Oussama <sadosami297@gmail.com>
2025-07-10 20:29:45 +0100

    first commit

---
+++ database/migrations/2025_07_10_183946_create_proprietaires_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('proprietaires', function (Blueprint $table) {
+            $table->id();
+            $table->string('phone')->unique();
+            $table->unsignedInteger("etage");
+            $table->unsignedInteger("numero_appartement");
+            $table->foreignId("immeuble_id")->constrained()->onDelete('cascade');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('proprietaires');
+    }
+};
