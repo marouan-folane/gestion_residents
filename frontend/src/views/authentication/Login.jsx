34c69881b1ce966e9084d7993a8612d593651b20
AIT-MOHAMED Oussama <sadosami297@gmail.com>
2025-07-12 17:08:09 +0100

    commit before pull

--- frontend/src/views/authentication/Login.jsx
+++ frontend/src/views/authentication/Login.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 
-import axios from "axios";
 import axiosClient from "../../axios-client";
 import { useStateContext } from "../../contexts/ContextProvider";
 
